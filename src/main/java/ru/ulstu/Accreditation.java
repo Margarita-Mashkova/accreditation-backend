264072125771834db451be772ef3debfbdf7bfd5
rita <margosha.mashkova@mail.ru>
2024-02-09 03:48:42 +0400

    generate project

---
+++ src/main/java/ru/ulstu/Accreditation.java
@@ -0,0 +1,13 @@
+package ru.ulstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accreditation {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accreditation.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ulstu/AccreditationTests.java
@@ -0,0 +1,13 @@
+package ru.ulstu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccreditationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
